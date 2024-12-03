1304101fab1c57c0dc88830cede40a44af357f74
haru-some <777tmdgus@naver.com>
2024-12-04 02:30:42 +0900

    241204 작업 진행
    241204 작업 진행

--- JAVA_PointMgr6/src/kr/co/iei/point/controller/PointController.java
+++ JAVA_PointMgr6/src/kr/co/iei/point/controller/PointController.java
@@ -1,8 +1,11 @@
 package kr.co.iei.point.controller;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Set;
 
 import kr.co.iei.point.dao.PointDao;
+import kr.co.iei.point.vo.Grade;
 
 public class PointController {
 	Scanner sc;
@@ -52,9 +55,32 @@ public class PointController {
 	}
 	
 	public void insertMember() {
-		System.out.print("등록 할 회원 등급 입력[silver/gold/vip] :  ");
-		String grade = sc.next();
+		while(true) {
+			System.out.print("등록 할 회원 등급 입력[silver/gold/vip] :  ");
+			String grade = sc.next();
+			if(!dao.gradeSetContains(grade)) {
+				System.out.println("올바른 등급을 입력해주세요.");
+				continue;
+			}else {
+				System.out.print("등록 할 회원 이름 입력 : ");
+				String name = sc.next();
+				System.out.print("등록 할 회원 포인트 입력 : ");
+				int point = sc.nextInt();
+				dao.insertMember(grade, name, point);
+				System.out.println("회원 등록 완료");
+				break;
+			}
+		}	
+	}//insertMember() 종료
+	
+	public void printAllMember() {
+		System.out.println("\n------------ 전체 회원 정보 출력 ------------\n");
+		System.out.println("등급\t이름\t포인트\t보너스");
+		System.out.println("------------------------------------------");
 		
+		for(String name : dao.getMemberList()) {
+			
+		}
 	}
 	
 }
--- JAVA_PointMgr6/src/kr/co/iei/point/dao/PointDao.java
+++ JAVA_PointMgr6/src/kr/co/iei/point/dao/PointDao.java
@@ -1,9 +1,14 @@
 package kr.co.iei.point.dao;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Set;
 
+import kr.co.iei.point.vo.Gold;
 import kr.co.iei.point.vo.Grade;
+import kr.co.iei.point.vo.Silver;
+import kr.co.iei.point.vo.Vip;
 
 public class PointDao {
 	HashMap<String, Grade> members;
@@ -11,12 +16,34 @@ public class PointDao {
 
 	public PointDao() {
 		super();
+		gradeSets.add("silver");
+		gradeSets.add("gold");
+		gradeSets.add("vip");
 		importMembers();
 		
 	}
 	
-	public void insertMember(Grade g) {
-		//members.put(gradeSets, g);
+	public boolean gradeSetContains(String g) {
+		return gradeSets.contains(g);
+	}
+	
+	public void insertMember(String grade, String name, int point) {
+		switch(grade) {
+		case "silver":
+			members.put(name, new Silver(grade, name, point));
+			break;
+		case "gold":
+			members.put(name, new Gold(grade, name, point));
+			break;
+		case "vip":
+			members.put(name, new Vip(grade, name, point));
+			break;
+		}
+	}
+	
+	public Set<String> getMemberList(){
+		Set<String> keySet = members.keySet();
+		return keySet;
 	}
 	
 	public void exportMembers() {
