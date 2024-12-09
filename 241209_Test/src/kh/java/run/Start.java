553ba6820d4ba0369b74e42c458e39070d4125a8
haru-some <777tmdgus@naver.com>
2024-12-09 18:21:06 +0900

    20241209 작업 완료
    20241209 작업 완료

---
+++ 241209_Test/src/kh/java/run/Start.java
@@ -0,0 +1,12 @@
+package kh.java.run;
+
+import kh.java.exam.Server;
+
+public class Start {
+
+	public static void main(String[] args) {
+		Server sv = new Server();
+		sv.ChattingServer();
+	}
+
+}
